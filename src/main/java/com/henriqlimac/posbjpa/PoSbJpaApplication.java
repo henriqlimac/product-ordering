f48d568b9c42a4da866c26c4d7397434a9233a1c
henriqlimac <h.cardosinho.lima@gmail.com>
2026-03-15 00:10:42 -0300

    first: project created

---
+++ src/main/java/com/henriqlimac/posbjpa/PoSbJpaApplication.java
@@ -0,0 +1,13 @@
+package com.henriqlimac.posbjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoSbJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoSbJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/henriqlimac/posbjpa/PoSbJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriqlimac.posbjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoSbJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
